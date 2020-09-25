86501bcacaee817b057eac6f3a16cd964c9dc334
Shivananda Sai <47454928+shivanandasai@users.noreply.github.com>
2020-09-25 14:27:30 +0530

    Add files via upload

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MainClass {
+    public static void main(String[] args){
+
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new SuperHumanBot());
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
